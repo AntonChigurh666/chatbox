b226013668c838176496acd62448c813e61e652e
AntonChigurh666 <anton.chigurh333@protonmail.com>
2025-01-19 09:36:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -97,3 +97,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+console.log("Entered password:", password);
+console.log("Expected password:", adminPassword);
